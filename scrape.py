98914dbc4884a4884f592a1bca2571aeba65c1dd
karolskoczyk <32647007+karolskoczyk@users.noreply.github.com>
2018-11-20 13:34:45 +0100

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -5,7 +5,7 @@ source = requests.get('https://www.otodom.pl/sprzedaz/mieszkanie/krakow/').text
 
 soup = BeautifulSoup(source, 'lxml')
 
-for mieszkanie in soup.find_all('div', class_='col-md-content section-listing__row-content'):
+for mieszkanie in soup.find_all('div', class_='offer-item-details'):
 
     nazwa = mieszkanie.find('span', class_='offer-item-title').text
     podpis = mieszkanie.find('p', class_='text-nowrap hidden-xs').text
@@ -13,7 +13,7 @@ for mieszkanie in soup.find_all('div', class_='col-md-content section-listing__r
     metry = mieszkanie.find('li', class_='hidden-xs offer-item-area').text
     cena_metr = mieszkanie.find('li', class_='hidden-xs offer-item-price-per-m').text
     cena = mieszkanie.find('li', class_='offer-item-price').text
-    oferta = mieszkanie.find('li', class_='pull-right').text
+
 
     print(nazwa)
     print(podpis)
@@ -21,5 +21,5 @@ for mieszkanie in soup.find_all('div', class_='col-md-content section-listing__r
     print("Metraz: " + metry)
     print("Cena za metr: " + cena_metr)
     print(cena)
-    print(oferta)
+
  
