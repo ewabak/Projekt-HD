336c5a839d1abca9f6ccbda78c99cbdbdcb0dbe5
karolskoczyk <32647007+karolskoczyk@users.noreply.github.com>
2018-11-24 13:24:39 +0100

    aktualizacja

--- scrape.py
+++ scrape.py
@@ -1,9 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
-source = requests.get('https://www.otodom.pl/sprzedaz/mieszkanie/krakow/').text
+source = requests.get('https://www.otodom.pl/sprzedaz/mieszkanie/krakow/')
 
-soup = BeautifulSoup(source, 'lxml')
+soup = BeautifulSoup(source.text, 'html.parser')
+
+csv_file = open('cms_scrape.csv', 'w', encoding='utf-8', errors = 'ignore')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['nazwa', 'podpis', 'pokoj', 'metry', 'cena_metr', 'cena'])
 
 for mieszkanie in soup.find_all('div', class_='offer-item-details'):
 
@@ -12,14 +17,23 @@ for mieszkanie in soup.find_all('div', class_='offer-item-details'):
     pokoj = mieszkanie.find('li', class_='offer-item-rooms hidden-xs').text
     metry = mieszkanie.find('li', class_='hidden-xs offer-item-area').text
     cena_metr = mieszkanie.find('li', class_='hidden-xs offer-item-price-per-m').text
-    cena = mieszkanie.find('li', class_='offer-item-price').text
+    cena = mieszkanie.find('li', class_='offer-item-price').text.replace(' ','').replace('\n','')
 
+    csv_writer.writerow([nazwa, podpis, pokoj, metry, cena_metr, cena])
 
-    print(nazwa)
-    print(podpis)
-    print(pokoj)
-    print("Metraz: " + metry)
-    print("Cena za metr: " + cena_metr)
-    print(cena)
+csv_file.close()
 
- 
+from flask import Flask, render_template
+    
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+
+    return render_template("index.html", nazwa = nazwa, pokoj = pokoj, podpis = podpis, metry = metry, cena_metr = cena_metr, cena = cena)
+
+if __name__ == "__main__":
+    app.run()
+
+    csv_writer.writerow([nazwa, podpis, pokoj])
+csv_file.close()
