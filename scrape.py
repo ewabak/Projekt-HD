d14b8336a20dc0091568c3e15472930179747a43
karolskoczyk <32647007+karolskoczyk@users.noreply.github.com>
2018-11-28 15:09:43 +0100

    petla

--- scrape.py
+++ scrape.py
@@ -3,11 +3,6 @@ import requests
 import csv
 import pandas as pd
 
-source = requests.get('https://www.otodom.pl/sprzedaz/mieszkanie/krakow/')
-
-soup = BeautifulSoup(source.text, 'html.parser')
-
-register = template.Library()
 
 csv_file = open('cms_scrape.csv', 'w', encoding='utf-8', errors = 'ignore')
 csv_writer = csv.writer(csv_file)
@@ -20,31 +15,36 @@ metryy=[]
 cena_metryy=[]
 ceny=[]
 
-for mieszkanie in soup.find_all('div', class_='offer-item-details'):
+for i in range(1, 10):
+    page = "https://www.otodom.pl/sprzedaz/mieszkanie/krakow/?page={}".format(i)
+    html = requests.get(page)
+    soup = BeautifulSoup(html.text, 'lxml')
+    
+    for mieszkanie in soup.find_all('div', class_='offer-item-details'):
 
-    nazwa = mieszkanie.find('span', class_='offer-item-title').text
-    nazwy.append(nazwa)
+        nazwa = mieszkanie.find('span', class_='offer-item-title').text
+        nazwy.append(nazwa)
 
-    podpis = mieszkanie.find('p', class_='text-nowrap hidden-xs').text
-    podpisy.append(podpis)
+        podpis = mieszkanie.find('p', class_='text-nowrap hidden-xs').text
+        podpisy.append(podpis)
 
-    pokoj = mieszkanie.find('li', class_='offer-item-rooms hidden-xs').text
-    pokoje.append(pokoj)
+        pokoj = mieszkanie.find('li', class_='offer-item-rooms hidden-xs').text
+        pokoje.append(pokoj)
 
-    metry = mieszkanie.find('li', class_='hidden-xs offer-item-area').text
-    metryy.append(metry)
+        metry = mieszkanie.find('li', class_='hidden-xs offer-item-area').text
+        metryy.append(metry)
 
-    cena_metr = mieszkanie.find('li', class_='hidden-xs offer-item-price-per-m').text
-    cena_metryy.append(cena_metr)
+        cena_metr = mieszkanie.find('li', class_='hidden-xs offer-item-price-per-m').text
+        cena_metryy.append(cena_metr)
 
-    cena = mieszkanie.find('li', class_='offer-item-price').text.replace(' ','').replace('\n','')
-    ceny.append(cena)
+        cena = mieszkanie.find('li', class_='offer-item-price').text.replace(' ','').replace('\n','')
+        ceny.append(cena)
 
 
-    csv_writer.writerow([nazwa, podpis, pokoj, metry, cena_metr, cena])
+        csv_writer.writerow([nazwa, podpis, pokoj, metry, cena_metr, cena])
 
-    #data = [[nazwa, podpis, pokoj, metry, cena_metr, cena]]
-    #df = pd.DataFrame(data, columns=['Nazwa', 'Podpis', 'Pokoj', 'Metry', 'Cena za metr', 'Cena'])
+        #data = [[nazwa, podpis, pokoj, metry, cena_metr, cena]]
+        #df = pd.DataFrame(data, columns=['Nazwa', 'Podpis', 'Pokoj', 'Metry', 'Cena za metr', 'Cena'])
 
 df = pd.DataFrame({'Nazwa':nazwy, 'Podpis':podpisy, 'Pokoj':pokoje, 'Metry':metryy, 'Cena za metr':cena_metryy, 'Cena':ceny})
     
